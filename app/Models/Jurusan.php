0dd6b262ad7d9e621e51a0aa53845ae64d1f6ea0
BabuTugas <gamingindonesia0768@gmail.com>
2023-01-12 13:19:32 +0700

    jurusan

--- app/Models/Jurusan.php
+++ app/Models/Jurusan.php
@@ -5,13 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Kelas;
+
 class Jurusan extends Model
 {
     use HasFactory;
     protected $table = "jurusan";
+    protected $fillable = ["nama_jurusan", "kode_jurusan"];
 
-      public function kelas()
+    public $timestamps = false;
+
+    public function kelas()
     {
-        return $this->belongsTo(Kelas::class,'id_jurusan');
+        return $this->belongsTo(Kelas::class, 'id_jurusan');
     }
 }
