fc214a59d13e3580b947fbbdd837c28a2327f05c
mochhengkyds <mochhengkyds@gmail.com>
2023-01-12 14:58:52 +0700

    modal

--- resources/views/dashboard/jurusan/index.blade.php
+++ resources/views/dashboard/jurusan/index.blade.php
@@ -21,6 +21,9 @@
                     <div class="card-body">
                     <div class="table-responsive">
                       <a href="/admin/jurusan"><button class="btn btn-primary m-3" type="submit">Tambah Data</button></a>
+                      <button type="button" class="btn btn-primary ml-3" data-toggle="modal" data-target="#exampleModalCenter">
+                        Import
+                      </button>
                       <table class="table table-bordered table-md">
                         <tr>
                           <th>No</th>
@@ -44,4 +47,29 @@
               </div>
             </div>
         </section>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Unggah file di sini</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="/import/jurusan" method="POST" enctype="multipart/form-data">
+          <input type="file" class="form-control">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
