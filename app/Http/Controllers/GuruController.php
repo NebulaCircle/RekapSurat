dad91f2b2f8891cefb755edec0b3e403711b78be
mochhengkyds <mochhengkyds@gmail.com>
2023-01-12 13:49:56 +0700

    terimakasih

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -24,9 +24,10 @@ class GuruController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Guru $guru)
     {
-        return view('dashboard.guru.create');
+        $guru = Guru::all();
+        return view('dashboard.guru.create', compact('guru'));
     }
 
     /**
@@ -45,7 +46,7 @@ class GuruController extends Controller
         ]);
 
         Guru::create($request->except('_token'));
-        return redirect('/admin/guru');
+        return redirect('/admin/guru')->with('pesan','kelas berhasil di tambahkan');
     }
 
     /**
@@ -67,7 +68,7 @@ class GuruController extends Controller
      */
     public function edit(Guru $guru)
     {
-        return view('dashboard.guru.edit');
+        return view('dashboard.guru.edit', compact('guru'));
     }
 
     /**
@@ -87,7 +88,7 @@ class GuruController extends Controller
         ]);
 
         $guru->update($request->except('_token'));
-        return redirect('/admin/guru');
+        return redirect('/admin/guru')->with('pesan','guru berhasil di tambah');
     }
 
     /**
